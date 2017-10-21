79b6668ebd9057f9b823551606f769a180e528c4
plyalyut <pavlo_lyalyutskyy@brown.edu>
2017-10-21 17:40:26 -0400

    php deploy

--- kennel.php
+++ kennel.php
@@ -8,4 +8,18 @@ $connectionOptions = array(
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
+//fetch all the form data
+$name = $_GET["demo-name"];
+$email = $_GET["demo-email"];
+
+//put data into server
+$data= "INSERT INTO shelter (ShelName, Email)
+VALUES ($name, $emaik)";
+
+$postResults= sqlsrv_query($conn, $tsql);
+echo ("Reading data from table" . PHP_EOL);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+
+sqlsrv_free_stmt($getResults);
 ?>
