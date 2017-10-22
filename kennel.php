f978188672bd2750b8a943656f897a1984bde8fd
plyalyut <pavlo_lyalyutskyy@brown.edu>
2017-10-22 01:52:01 -0400

    test error fix

--- kennel.php
+++ kennel.php
@@ -66,9 +66,9 @@
 <?php
 $serverName = "snoop.database.windows.net";
 $connectionOptions = array(
-    "Database" => "Snoop",
-    "Uid" => "snoop",
-    "PWD" => "hackharvard20!7"
+		"Database" => "Snoop",
+		"Uid" => "snoop",
+		"PWD" => "hackharvard20!7"
 );
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
